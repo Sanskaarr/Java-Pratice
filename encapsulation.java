c020d95f6a046ea58ae324e7eaf6aa9ae2f72fcc
Sanskar <Sanskar0808jain@gmail.com>
2024-10-13 02:32:15 +0530

    Git Changed Encapsulation Program

--- encapsulation.java
+++ encapsulation.java
@@ -1,44 +1,46 @@
-// class Integernumber{
-//     private int number;
+public class encapsulation {
+    public static void main(String[] args) {
+        Integernumber obj = new Integernumber();
+        obj.setName(100);
+        System.out.println(obj.getName());
+    }
+}
 
-// public int getName(){
-//     return number;
-// }
+class Integernumber{
+    private int number=70;
 
-// public void setName(int number){
-//     this.number = number;
-// }
-// }
+public int getName(){
+    return number;
+}
 
-// public class encapsulation {
-//     public static void main(String[] args) {
-//         Integernumber obj = new Integernumber();
-//         obj.setName(100);
-//         System.out.println(obj.getName());
+public void setName(int no){
+    this.number = no;
+}
+ }
+
+
+
+// import java.util.Scanner;
+
+// class Number{
+//     private int no;
+
+//     public int getno(){
+//         return no;
+//     }
+
+//     public void setNo(int number){
+//         this.no = number;
 //     }
 // }
 
-import java.util.Scanner;
-
-class Number{
-    private int no;
-
-    public int getno(){
-        return no;
-    }
-
-    public void setNo(int number){
-        this.no = number;
-    }
-}
-
-public class encapsulation {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        Number obj = new Number();
-        System.out.println("Enter the Number: ");
-        int a = sc.nextInt();
-        obj.setNo(a);
-        System.out.println("The Number is: "+ obj.getno());
-    }
-}
+// public class encapsulation {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         Number obj = new Number();
+//         System.out.println("Enter the Number: ");
+//         int a = sc.nextInt();
+//         obj.setNo(a);
+//         System.out.println("The Number is: "+ obj.getno());
+//     }
+// }
